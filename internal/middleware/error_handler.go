2f710e65323f8e02a0a2a024304513ec1bb88e4f
Nguyễn Văn Đức <duc@Mac-mini-cua-Nguyen.local>
2025-02-07 11:38:26 +0700

    fix error handler

--- internal/middleware/error_handler.go
+++ internal/middleware/error_handler.go
@@ -3,34 +3,36 @@ package middleware
 import (
 	"fmt"
 	"net/http"
-    "github.com/gin-gonic/gin"
+
+	"github.com/gin-gonic/gin"
 
 	"github.com/ducnv194023/shoppe_be_go/pkg/errors"
 )
 
 func ErrorHandler() gin.HandlerFunc {
-    return func(c *gin.Context) {
-        defer func() {
-            if err := recover(); err != nil {
-                // Log error
-                fmt.Printf("panic occurred: %v\n", err)
+	return func(c *gin.Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				// Log error
+				fmt.Printf("panic occurred: %v\n", err)
 
-                appErr := &errors.AppError{
-                    Code:    http.StatusInternalServerError,
-                    Message: "Internal Server Error",
-                    Err:     fmt.Errorf("%v", err), 
-                }
+				appErr := &errors.AppError{
+					Code:    http.StatusInternalServerError,
+					Message: "Internal Server Error",
+					Err:     fmt.Errorf("%v", err),
+				}
 
-                c.JSON(appErr.Code, gin.H{
-                    "error":   appErr.Message,
-                    "code":    appErr.Code,
-                    "success": false,
-                })
+				c.JSON(appErr.Code, gin.H{
+					"error":   appErr.Message,
+					"details": appErr.Err.Error(),
+					"code":    appErr.Code,
+					"success": false,
+				})
 
-                c.Abort()
-            }
-        }()
+				c.Abort()
+			}
+		}()
 
-        c.Next()
-    }
+		c.Next()
+	}
 }
